9ac70911d5c586aa55ae0eb3aa101f74197ef408
Nils Petzall <nils.petzall@gmail.com>
2022-10-10 00:39:09 +0200

    chore(dev): prettier

--- dist/index.js
+++ dist/index.js
@@ -2693,10 +2693,10 @@ exports["default"] = _default;
 
 let wait = function (milliseconds) {
   return new Promise((resolve) => {
-    if (typeof milliseconds !== 'number') {
-      throw new Error('milliseconds not a number');
+    if (typeof milliseconds !== "number") {
+      throw new Error("milliseconds not a number");
     }
-    setTimeout(() => resolve("done!"), milliseconds)
+    setTimeout(() => resolve("done!"), milliseconds);
   });
 };
 
@@ -2837,18 +2837,17 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(186);
 const wait = __nccwpck_require__(312);
 
-
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
+    const ms = core.getInput("milliseconds");
     core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.info(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
